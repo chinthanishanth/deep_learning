42a0de82aeed6d98c51b5172789d60006d8ec408
chintha nishanth <chinthanishanth@gmail.com>
2020-01-09 12:49:06 +0530

    modified install_python3.7.sh to set 3.7 as default in gcp debian 9 instance

--- install_python3.7.sh
+++ install_python3.7.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
-
 sudo apt update \
-&& sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y\
-&& curl -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz \
-&& tar -xf Python-3.7.3.tar.xz \
-&& cd Python-3.7.3 \
-&& ./configure \
-&& make -j 8 \
-&& sudo make altinstall
+        && sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadli
+ne-dev libffi-dev wget -y\
+        && curl -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz \
+        && tar -xf Python-3.7.3.tar.xz \
+        && cd Python-3.7.3 \
+        && ./configure \
+        && make -j 8 \
+        && sudo make altinstall \
+        && sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1\
+        && sudo update-alternatives --install /usr/bin/python python  /usr/local/bin/python3.7 2\
+        && sudo update-alternatives --config python
