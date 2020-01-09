37b552ac5b46fe89a676dc4b892aa80fe2d99ab6
chintha nishanth <chinthanishanth@gmail.com>
2020-01-09 08:09:41 +0000

    fixed error due to newline

--- install_python3.7.sh
+++ install_python3.7.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 sudo apt update \
-        && sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadli
-ne-dev libffi-dev wget -y\
+        && sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y\
         && curl -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz \
         && tar -xf Python-3.7.3.tar.xz \
         && cd Python-3.7.3 \
